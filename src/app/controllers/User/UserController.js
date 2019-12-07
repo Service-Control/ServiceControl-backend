8a2895f34a85f6db65f2778d2549470f93be823f
Lucas Correa <lucas.dcorrea1@gmail.com>
2019-12-07 00:48:54 -0300

    initial commit

--- src/app/controllers/User/UserController.js
+++ src/app/controllers/User/UserController.js
@@ -1,31 +1,18 @@
-const userRepository = require('../../repository/User/UserRepository')
+
+const userService = require('../../services/User/UserServices');
 
 module.exports = {
     async getAll(req, res) {
-
-        const users = await userRepository.getAll();
-
+        const users = userService.get();
+        
         return res.json(users);
     },
 
     async regiter(req, res) {
         const { name, email, cpf, password } = req.body;
+        const data = { name, email, cpf, password };
+        const user = userService.post(data);
 
-        const userEmail = await userRepository.getEmail(email);
-        console.log(userEmail)
-        if (userEmail)
-            return res.status(400).send({
-                error: "Email já cadastrado!"
-            });
-
-        const userCpf = await userRepository.getCpf(cpf);
-        if (userCpf)
-            return res.status(400).send({
-                error: "Cpf já cadastrado!"
-            });
-
-
-        const user = await userRepository.post(name, email, cpf, password);
         return res.json(user);
     },
 };
